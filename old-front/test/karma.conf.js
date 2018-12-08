2ea7d834be93f54cd846088dff7efd255dc14ce3
Luis Bustos <lpbustoscarrasco@gmail.com>
2018-12-08 01:18:03 -0300

    Add new frontend to app based in Vue.js . Add autocomplete function.

--- old-front/test/karma.conf.js
+++ old-front/test/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
     autoWatch: true,
 
     // base path, that will be used to resolve files and exclude
-    basePath: '../',
+    basePath: '../..',
 
     // testing framework to use (jasmine/mocha/qunit/...)
     // as well as any additional frameworks (requirejs/chai/sinon/...)
@@ -32,9 +32,9 @@ module.exports = function(config) {
       'bower_components/underscore/underscore.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      '../app/scripts/**/*.js',
+      '/mock/**/*.js',
+      '/spec/**/*.js'
     ],
 
     // list of files / patterns to exclude
