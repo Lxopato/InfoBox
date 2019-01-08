09d2110f9da8efff44342b456e1a140f2ee4bf9f
Luis Bustos <lpbustoscarrasco@gmail.com>
2019-01-07 22:55:47 -0300

    Add footer, replaced favicon.ico

--- src/components/Searcher.vue
+++ src/components/Searcher.vue
@@ -1,61 +1,73 @@
 <template>
     <div>
-    <div class="form">
-        <h1>Wikipedia Infobox Generator</h1>
-        <h4>Select Language and Strategy</h4>
-        <form>
-            <label>Language: </label>
-            <select v-model="language">
-                <option v-for="language in languages" v-bind:key="language.value">
-                    {{language.value}}
-                </option>
-            </select>
-            <br/>
-            <label>Strategy: </label>
-            <select v-model="strategy">
-                <option v-for="strategy in strategies" v-bind:key="strategy.val">
-                    {{strategy.val}}
-                </option>
-            </select>
-            <br/>
-            <button v-on:click="refresh_infobox" type="button">Refresh Infobox</button>
-        </form>
-        <br/>
-    </div>
-    <div>
-        <br>
-        <h4>Search Entity in the Selected Language</h4>
-        <cool-select
-            v-model="selected"
-            :items="items"
-            item-text="id"
-            placeholder="Enter entity name"
-            disable-filtering-by-search
-            @search="onSearch"
-            >
-            <template slot="no-data">
-                {{noData ? "No information found by request":"No entity queried"}}
-            </template>
-            <template slot="item" slot-scope="{ item }">
-                <div style="display: flex; align-items: center;">
-                    <div>
-                        <span class="item-name"> {{ item.match.text }} ({{ item.id}}) </span><br />
-                        <span class="item-domain"> {{ item.description }} </span>
-                    </div>
-                </div>
+        <div class="container">
+            <div class="form">
+                <h1>Wikipedia Infobox Generator</h1>
+                <h4>Select Language and Strategy</h4>
+                <form>
+                    <label>Language: </label>
+                    <select v-model="language">
+                        <option v-for="language in languages" v-bind:key="language.value">
+                            {{language.value}}
+                        </option>
+                    </select>
+                    <br/>
+                    <label>Strategy: </label>
+                    <select v-model="strategy">
+                        <option v-for="strategy in strategies" v-bind:key="strategy.val">
+                            {{strategy.val}}
+                        </option>
+                    </select>
+                    <br/>
+                    <button v-on:click="refresh_infobox" type="button">Refresh Infobox</button>
+                </form>
+                <br/>
+            </div>
+            <div>
+                <br>
+                <h4>Search Entity in the Selected Language</h4>
+                <cool-select
+                        v-model="selected"
+                        :items="items"
+                        item-text="id"
+                        placeholder="Enter entity name"
+                        disable-filtering-by-search
+                        @search="onSearch"
+                >
+                    <template slot="no-data">
+                        {{noData ? "No information found by request":"No entity queried"}}
+                    </template>
+                    <template slot="item" slot-scope="{ item }">
+                        <div style="display: flex; align-items: center;">
+                            <div>
+                                <span class="item-name"> {{ item.match.text }} ({{ item.id}}) </span><br />
+                                <span class="item-domain"> {{ item.description }} </span>
+                            </div>
+                        </div>
 
-            </template>
-        </cool-select>
-        <br>
-        <br>
+                    </template>
+                </cool-select>
+                <br>
+                <br>
+            </div>
+            <div v-if="show">
+                <Infobox
+                        :lang= "language"
+                        :strat= "strategy"
+                        :id="selected.id.substr(1)">
+
+                </Infobox>
+            </div>
         </div>
-        <div v-if="show">
-            <Infobox
-                    :lang= "language"
-                    :strat= "strategy"
-                    :id="selected.id.substr(1)">
-
-            </Infobox>
+        <br>
+        <div class="footer">
+            Paper: <a href="http://wikiworkshop.org/2018/papers/wikiworkshop2018_paper_17.pdf">Automatically Generating Wikipedia Info-boxes from Wikidata</a>
+            <br>
+            <br>
+            Website created by: <a href="https://github.com/Lxopato">Luis Patricio Bustos</a>.
+            <br>
+            Source Code: <a href="https://github.com/Lxopato/InfoBox">Frontend</a>, <a href="https://github.com/Lxopato/infobox-api">Backend</a>
+            <br>
         </div>
     </div>
 </template>
@@ -138,5 +150,19 @@
     }
     .item-domain {
         color: grey;
+    }
+    .footer {
+        clear: both;
+        position: relative;
+        height: 40px;
+        margin-top: -40px;
+        text-align: center;
+        padding-top: 15px;
+    }
+    .container {
+        position: relative;
+        margin: 0;
+        padding-bottom: 6rem;
+        min-height: 100%;
     }
 </style>
